7eaf0dfecc153c8fe5e02436c3e0010d700f350f
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-07-18 14:39:45 +0200

    add start function

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,13 @@ start.addEventListener("click", () => starting())
 function starting() {
     console.log("hello world")
     console.log(parseInt(roller.value))
-    console.log(char.value)
+    console.log(parseInt(char.value))
     console.log(proficent.value)
+    getRandomInt(1, parseInt(roller.value) + 1)
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
